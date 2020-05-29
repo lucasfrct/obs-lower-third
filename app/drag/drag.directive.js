8e96da89abd1a8e6cb1d623e93121be06c987f39
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-05-29 10:42:20 -0300

    Correção do Bug Reset Position
    Corrigido o problema de reset na posicaão do texto e imagem no primeiro carregamento

--- app/drag/drag.directive.js
+++ app/drag/drag.directive.js
@@ -22,16 +22,17 @@
         
         function link($scope, element, attr) {
 
-            var position = { x: 0, y: 0, limit: 3 }
-            
-            var start = { x: 0, y: 0 }
-            
-            var show = { x: 0, y: 0, width: 0, height: 0, borderInit: 0, borderEnd: 0 }
-            
             var container = element.parent()
 
-            show.width = container[0].clientWidth
+            var orientation = { x: 0, y: 0}
 
+            var start = { ...orientation }
+
+            var position = { limit: 3, ...orientation }
+            
+            var show = { width: 0, height: 0, borderInit: 0, borderEnd: 0, ...orientation }
+
+            show.width = container[0].clientWidth
             show.heigth = container[0].clientHeight
             
             show.borderInit = position.limit
@@ -48,26 +49,20 @@
                 start.y = (event.screenY - show.y);
                 start.x = (event.screenX - show.x);
 
+                Apply(Position(event))
+
                 $document.on('mousemove', mousemove);
                 $document.on('mouseup', mouseup);
 
             });
     
-            function mousemove(event) {
-                
-                //console.log("DRAG MOVE")
-                position = Position(event)
-
-                element.css({ top: position.y+'%', left: position.x+"%" });
-                
-            }
+            function mousemove(event) { Apply(Position(event)) }
             
             function mouseup() {
-
             
                 $document.off('mousemove', mousemove);
                 $document.off('mouseup', mouseup);
-
+                
                 if(element[0].nodeName == "SPAN") {
                     $scope.lower.text.x = position.x
                     $scope.lower.text.y = position.y
@@ -75,9 +70,9 @@
                     $scope.lower.image.x = position.x
                     $scope.lower.image.y = position.y
                 }
-
-                $scope.$apply()
+    
                 $scope.updade($scope.lower)
+                $scope.$apply()
             }
 
             function Position(event){
@@ -87,17 +82,21 @@
 
                 position.x = Number((100 / show.width) * show.x)
                 position.y = Number((100 / show.heigth) * show.y)
-
-                console.log("position", position)
-
+                
                 // Limitando as bordas
-
+                
                 if (position.x <= show.borderInit) { position.x = show.borderInit }
                 if (position.y <= show.borderInit) { position.y = show.borderInit }
                 if (position.x >= show.borderEnd) { position.x = show.borderEnd }
                 if (position.y >= show.borderEnd) { position.y = show.borderEnd }
+                
+                console.log("position", position)
 
                 return position
+            }
+
+            function Apply(position) {
+                element.css({ top: String(position.y)+'%', left: String(position.x)+"%" });
             }
         
         }
